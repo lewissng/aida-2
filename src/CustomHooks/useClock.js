791492563580c53bb6b1746a694dab3b1bb5b791
Lewis <lewissngpt@gmail.com>
2021-08-10 12:58:30 +0800

    custom hook update 2

--- src/CustomHooks/useClock.js
+++ src/CustomHooks/useClock.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 
 const useClock = (time) => {
+
+    /**
+     * This is a very rudimentary custom hook.
+     * I understand that custom hooks should have more logics
+     * and state etc.
+     */
+    
     useEffect(() => {
         time();
         return () => {
